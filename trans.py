2457aa18e18a3bfeaf21332d72bb857a94fedc06
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-03-03 17:58:00 +0530

    Update translater.py

--- trans.py
+++ trans.py
@@ -1,6 +1,8 @@
 import os
 from pyfiglet import Figlet
 from translate import Translator
+from tqdm import tqdm
+import time
 translator= Translator(to_lang="German")
 os.system("clear")
 f=Figlet(font ='slant')
@@ -11,6 +13,8 @@ from gtts import gTTS
 language = 'en'
 output1=gTTS(text=translation,lang=language)
 output1.save('trans.mp3')
+for i in tqdm(range(100)):
+    time.sleep(0.2)
 os.system("mpv /data/data/com.termux/files/home/trans.mp3")
 
 
